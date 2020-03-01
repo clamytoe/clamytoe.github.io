e1e86a9a77c72b98e84b597e9781a4e041aa94c1
Martin Uribe <clamytoe@gmail.com>
2020-03-01 09:13:37 -0600

    Added some plugins, updated gitignore file, and made some changes to the config file.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+THEME = (
+    "/home/mohh/anaconda3/envs/ghpages/lib/python3.8/site-packages/pelican/themes/mg"
+)
+
+# Site and name settings
 AUTHOR = "Martin Uribe"
 SITENAME = "Ramblings"
 ALT_NAME = "#! " + SITENAME
@@ -14,29 +19,24 @@ SITEURL = ""
 FAVICON = "favicon.ico"
 FAVICON_TYPE = "image/vnd.microsoft.icon"
 
-META_IMAGE = SITEURL + "/img/clamytoe.png"
-META_IMAGE_TYPE = "image/png"
+# plugins
+MARKUP = ('md', 'ipynb')
+PLUGIN_PATHS = ['./plugins', './pelican-plugins']
+PLUGINS = ['ipynb.markup', 'render_math', 'better_codeblock_line_numbering']
+MARKDOWN_EXTENSIONS = [
+    'codehilite(css_class=highlight,linenums=False)',
+    'extra'
+]
 
+# Path settings
 PATH = "content"
-STATIC_PATHS = ["content/static"]
+STATIC_PATHS = ["static"]
+# STATIC_PATHS = ["content/static"]
 
+# Time and Date settings
 TIMEZONE = "US/Central"
-
 DEFAULT_LANG = "en"
 LOCALE = "en_US.UTF-8"
-
-THEME = (
-    "/home/mohh/anaconda3/envs/ghpages/lib/python3.8/site-packages/pelican/themes/mg"
-)
-
-# Social widget
-SOCIAL = (
-    ("twitter", "https://twitter.com/clamytoe"),
-    ("github", "https://github.com/clamytoe"),
-    ("linkedin", "https://www.linkedin.com/in/martin-uribe/"),
-)
-
-SHARE = True
 
 FOOTER = (
     "&copy; 2020 Martin Uribe. All rights reserved.<br>"
@@ -45,11 +45,13 @@ FOOTER = (
     "The MIT License</a>, unless otherwise specified."
 )
 
-
-DEFAULT_PAGINATION = 10
-
+# URL settings
 TAG_SAVE_AS = ""
 AUTHOR_SAVE_AS = ""
+ARTICLE_URL = 'articles/{date:%Y}/{date:%b}/{date:%d}/{slug}'
+ARTICLE_SAVE_AS = 'articles/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
 DIRECT_TEMPLATES = ("index", "categories", "archives", "search", "tipue_search")
 TIPUE_SEARCH_SAVE_AS = "tipue_search.json"
 
@@ -58,13 +60,26 @@ RELATIVE_URLS = False
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
-# TRANSLATION_FEED_ATOM = None
-# AUTHOR_FEED_ATOM = None
-# AUTHOR_FEED_RSS = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
+# Page settings
 TWITTER_USERNAME = "clamytoe"
 # DISQUS_SITENAME = "devsbytes"
 # SC_PROJECT = '10224955'
 # SC_SECURITY = '1f2cc438'
+
+# Social widget
+SOCIAL = (
+    ("twitter", "https://twitter.com/clamytoe"),
+    ("github", "https://github.com/clamytoe"),
+    ("linkedin", "https://www.linkedin.com/in/martin-uribe/"),
+)
+META_IMAGE = SITEURL + "/img/clamytoe.png"
+META_IMAGE_TYPE = "image/png"
+SHARE = True
+
+DEFAULT_PAGINATION = 10
